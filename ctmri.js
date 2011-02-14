9b7d532c897f6af936614eab266a3d184661fd52
unknown <csun11@.otsads.iit.edu>
2011-02-13 22:44:26 -0600

    Added jQuery effects: control the visibility of CT and MRI data forms with checkboxes; show data of the search results using Ajax

--- ctmri.js
+++ ctmri.js
@@ -13,10 +13,39 @@ $(document).ready(function(){
 //this js can only turn the background to black, but I cannot make the background back to white again when user clicks somewhere outside the login field. Why??
 
 	$(window).scroll(function() {
-		$('#leftpanel')
+		$('#nav')
 			.stop()
 			.animate({top: $(document).scrollTop()}, 'slow', 'easeOutBack');
 	});
 
+//use checkboxes to control the visibility of CT/MRI fieldset 
+	$('input#ct').click(function() {
+		$('.ctdata').toggle();
+		});
 
+	$('input#mri').click(function() {
+		$('.mridata').toggle();
+		});
+
+
+//showing results only when AJAX cannot function
+	$('#noajax').hide();
+		
 });
+
+//adding AJAX to the result page
+function loadXMLDoc() {
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+		{// code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	  }
+	else
+		{// code for IE6, IE5
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	  }
+	
+	xmlhttp.open("GET","fever.txt",false);
+	xmlhttp.send();
+	document.getElementById("ajax").innerHTML=xmlhttp.responseText;
+}
